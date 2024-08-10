61988bfc3a87e91ec43b558358de85dfbbdfa96a
xushicai123 <xushicai@proton.me>
2024-08-10 15:37:51 +0800

    Update Button.vue

--- src/components/button/Button.vue
+++ src/components/button/Button.vue
@@ -1,12 +1,11 @@
 <template>
-  <button class="jsl-button" :class="computedClass" :style="computedStyle" @click="click"
-    @touchstart="touchstart">
+  <button class="jsl-button" :class="computedClass" :style="computedStyle" @click="click" @touchstart="touchstart">
     <slot></slot>
   </button>
 </template>
 
 <script setup lang="ts">
-import { PropType ,computed } from 'vue';
+import { PropType, computed } from 'vue';
 const props = defineProps({
   size: {
     type: String as PropType<"large" | "normal" | "small" | "mini">,
@@ -39,20 +38,22 @@ const touchstart = () => {
   emit("touchstart");
 }
 
-const computedClass = computed(()=>{
-  return `
-    jsl-button--${props.size} 
-    jsl-button--${props.type} 
-    ${props.plain ? 'jsl-button--plain' : ''}
-    ${props.round ? 'jsl-button--round' : ''}
-    ${props.color ? 'jsl-button--color' : ''}
-    ${props.disabled ? 'jsl-button--disabled' : ''}
-  `
+const computedClass = computed(() => {
+  return [
+    `jsl-button--${props.size}`,
+    `jsl-button--${props.type}`,
+    props.plain ? "jsl-button--plain" : "",
+    props.round ? "jsl-button--round" : "",
+    props.color ? "jsl-button--color" : "",
+    props.disabled ? "jsl-button--disabled" : "",
+  ]
 })
-const computedStyle = computed(()=>{
-  return props.color ? `--jsl-customize-button-color:${props.color}` : ''
+const computedStyle = computed(() => {
+  return props.color ? `--jsl-customize-button-color:${props.color}` : ""
 })
 </script>
 
-<script lang="ts">export default { name: "JslButton" }</script>
+<script lang="ts">
+export default { name: "JslButton" }
+</script>
 <style scoped lang="less" src="./index.less"></style>
